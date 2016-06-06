8e27017fb5d88b310c6a9c8d203b9fa4895be369
hackwaly <hackwaly@gmail.com>
2016-06-07 00:36:38 +0800

    Implement goto def and hover

--- src/extension.ts
+++ src/extension.ts
@@ -6,9 +6,30 @@ import {OCamlMerlinSession} from './merlin';
 export function activate(context: vscode.ExtensionContext) {
     let configuration = vscode.workspace.getConfiguration("ocaml");
     let session = new OCamlMerlinSession();
-    
+
+    let syncBuffer = async (document, token) => {
+        await session.request(['checkout', 'auto', document.fileName]);
+        if (token.isCancellationRequested) return null;
+
+        await session.request(['seek', 'exact', {line: 1, col: 0}]);
+        if (token.isCancellationRequested) return null;
+
+        await session.request(['tell', 'source-eof', document.getText()]);
+        if (token.isCancellationRequested) return null;
+    };
+
+    let toVsPos = (pos) => {
+        return new vscode.Position(pos.line - 1, pos.col);
+    };
+    let fromVsPos = (pos: vscode.Position) => {
+        return {line: pos.line + 1, col: pos.character};
+    };
+    let toVsRange = (start, end) => {
+        return new vscode.Range(toVsPos(start), toVsPos(end));
+    };
+
     context.subscriptions.push(session);
-    
+
     context.subscriptions.push(
         vscode.languages.registerCompletionItemProvider({language: 'ocaml'}, {
             async provideCompletionItems(document, position, token) {
@@ -18,16 +39,8 @@ export function activate(context: vscode.ExtensionContext) {
                 ));
                 let prefix = /[A-Za-z_][A-Za-z_'0-9]*(?:\.[A-Za-z_][A-Za-z_'0-9]*)*\.?$/.exec(line)[0];
 
-                await session.request(['checkout', 'auto', document.fileName]);
-                if (token.isCancellationRequested) return null;
-
-                await session.request(['seek', 'exact', {line: 1, col: 0}]);
-                if (token.isCancellationRequested) return null;
-
-                await session.request(['tell', 'source-eof', document.getText()]);
-                if (token.isCancellationRequested) return null;
-
-                let result = await session.request(['complete', 'prefix', prefix, 'at', {line: position.line, col: position.character}]);
+                await syncBuffer(document, token);
+                let result = await session.request(['complete', 'prefix', prefix, 'at', fromVsPos(position)]);
                 if (token.isCancellationRequested) return null;
 
                 return new vscode.CompletionList(result.entries.map(({name, kind, desc, info}) => {
@@ -47,7 +60,7 @@ export function activate(context: vscode.ExtensionContext) {
                             case "class": return vscode.CompletionItemKind.Class;
                         }
                     };
-                    
+
                     completionItem.kind = kindFromMerlin(kind.toLowerCase());
                     completionItem.detail = desc;
                     completionItem.documentation = info;
@@ -55,16 +68,66 @@ export function activate(context: vscode.ExtensionContext) {
                 }));
             }
         }, '.'));
-    
+
+    context.subscriptions.push(
+        vscode.languages.registerDefinitionProvider({language: 'ocaml'}, {
+            async provideDefinition(document, position, token) {
+                await syncBuffer(document, token);
+
+                for (let kind of ['ml', 'mli']) {
+                    let result = await session.request(['locate', null, 'ml', 'at', fromVsPos(position)]);
+                    if (token.isCancellationRequested) return null;
+
+                    if (typeof result === 'string') {
+                        console.log(result);
+                        continue;
+                    }
+
+                    let uri = document.uri;
+                    let {file, pos} = result;
+                    if (file) {
+                        uri = vscode.Uri.file(file);
+                    }
+                    return new vscode.Location(uri, toVsPos(pos));
+                }
+
+                return null;
+            }
+        })
+    );
+
+    context.subscriptions.push(
+        vscode.languages.registerHoverProvider({language: 'ocaml'}, {
+            async provideHover(document, position, token) {
+                await syncBuffer(document, token);
+
+                let result = await session.request(['type', 'enclosing', 'at', fromVsPos(position)]);
+                if (token.isCancellationRequested) return null;
+
+                if (result.length <= 0) return;
+
+                let {start, end, type} = result[0];
+
+                if (type.startsWith('sig')) {
+                    let lines = type.split(/\n/g);
+                    if (lines.length > 6) {
+                        let end = lines.pop();
+                        lines = lines.slice(0, 5);
+                        lines.push('  (* ... *)');
+                        lines.push(end);
+                    }
+                    type = lines.join('\n');
+                } else {
+                    type = `type t = ${type}`;
+                }
+
+                return new vscode.Hover({language: 'ocaml', value: type}, toVsRange(start, end));
+            }
+        })
+    );
+
     let provideLinter = async (document, token) => {
-        await session.request(['checkout', 'auto', document.fileName]);
-        if (token.isCancellationRequested) return null;
-
-        await session.request(['seek', 'exact', {line: 1, col: 0}]);
-        if (token.isCancellationRequested) return null;
-
-        await session.request(['tell', 'source-eof', document.getText()]);
-        if (token.isCancellationRequested) return null;
+        await syncBuffer(document, token);
 
         let result = await session.request(['errors']);
         if (token.isCancellationRequested) return null;
@@ -82,28 +145,26 @@ export function activate(context: vscode.ExtensionContext) {
                 }
             };
             return new vscode.Diagnostic(
-                new vscode.Range(
-                    new vscode.Position(start.line, start.col),
-                    new vscode.Position(end.line, end.col)), 
-                message, 
+                toVsRange(start, end),
+                message,
                 fromType(type.toLowerCase()));
         });
     };
-    
+
     let diagnosticCollection = vscode.languages.createDiagnosticCollection('ocaml');
     let linterCancellationTokenSource: vscode.CancellationTokenSource;
     let linterDebounceTimer: number;
-    
+
     vscode.workspace.onDidChangeTextDocument(({document}) => {
         if (document.languageId !== 'ocaml') return;
-        
+
         clearTimeout(linterDebounceTimer);
         linterDebounceTimer = setTimeout(async () => {
             if (linterCancellationTokenSource) {
                 linterCancellationTokenSource.cancel();
             }
             linterCancellationTokenSource = new vscode.CancellationTokenSource();
-            
+
             let diagnostics = await provideLinter(document, linterCancellationTokenSource.token);
             diagnosticCollection.set(document.uri, diagnostics);
         }, configuration.get<number>('lintDelay'));
